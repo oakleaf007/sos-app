c4262ce6b26d459996d53b64323b5bf20b646819
OakLeaf007 <jjjbbbb736@gmail.com>
2026-03-17 18:57:59 +0530

    feat: added nearby place searching api

---
+++ src/controllers/nearbyFetch.js
@@ -0,0 +1,23 @@
+
+
+export const nearbyFetch = async(req, res)=>{
+    const {query, lat, lon} = req.query;
+    
+    try{
+        if(!lat || !lon || !query) return res.json({message: "No lat, lon recieved"});
+        
+        const latNum = parseFloat(lat);
+        const lonNum = parseFloat(lon);
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}&bounded=1&viewbox=${lonNum - 0.02},${latNum + 0.02},${lonNum + 0.02},${latNum - 0.02}`,{
+            headers:{"User-Agent": "sos-beacon"}
+        });
+        const data = await response.json();
+         console.log(data);
+       return  res.status(200).json(data);
+       
+
+
+    }catch(error){
+        res.status(500).json({error: "Geofetching failed"+ error.message})
+    }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 
-
+import { nearbyFetch } from "../controllers/nearbyFetch.js";
 const route = express.Router();
 
 
 route.get("/test",(req, res)=>{
     res.send("api route is working");
 });
+
+route.get("/nearbyfetch",nearbyFetch);
 
 
 export default route;
